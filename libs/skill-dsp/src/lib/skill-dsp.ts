61d7e68304862624eaf507cebe760f9696da11c8
Marc Buils <marc.buils@pinser.world>
2024-06-11 15:57:20 +0200

    feat: update skill-dsp

--- libs/skill-dsp/src/lib/skill-dsp.ts
+++ libs/skill-dsp/src/lib/skill-dsp.ts
@@ -1,27 +1,78 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { PinsSettings, executePinsList } from '@digipair/engine';
-
-const OLLAMA_SERVER = process.env['OLLAMA_SERVER'] ?? 'http://localhost:11434';
 
 class DspService {
   async model(params: any, _pinsSettingsList: PinsSettings[], _context: any) {
     const { AI } = await eval(`import('llmclient')`);
+    const { name, options } = params;
+
+    const modelInstance = AI(name, options);
+
+    return modelInstance;
+  }
+
+  async modelOpenAI(params: any, _pinsSettingsList: PinsSettings[], context: any) {
+    const { OpenAI } = await eval(`import('llmclient')`);
     const {
-      apiKey = 'none',
-      modelName = 'mistral',
-      temperature = 0,
-      keepAlive = 0,
-      baseUrl = OLLAMA_SERVER,
-      debug = false,
+      apiKey = context.privates.OPENAI_API_KEY ?? process.env['OPENAI_API_KEY'],
+      apiURL = context.privates.OPENAI_SERVER ?? process.env['OPENAI_SERVER'],
+      config,
+      options,
     } = params;
 
-    const model = AI('openai', {
+    const modelInstance = OpenAI({
       apiKey,
-      apiURL: baseUrl + '/v1',
-      config: { model: modelName, temperature, keepAlive },
-      options: { debug },
-    } as any);
+      apiURL,
+      config,
+      options,
+    });
 
-    return model;
+    return modelInstance;
+  }
+
+  async modelAzureOpenAi(params: any, _pinsSettingsList: PinsSettings[], context: any) {
+    const { AzureOpenAi } = await eval(`import('llmclient')`);
+    const {
+      apiKey = context.privates.AZURE_OPENAI_API_KEY ?? process.env['AZURE_OPENAI_API_KEY'],
+      resourceName = context.privates.AZURE_OPENAI_API_INSTANCE_NAME ??
+        process.env['AZURE_OPENAI_API_INSTANCE_NAME'],
+      deploymentName = context.privates.AZURE_OPENAI_API_DEPLOYMENT_NAME ??
+        process.env['AZURE_OPENAI_API_DEPLOYMENT_NAME'],
+      version = context.privates.AZURE_OPENAI_API_VERSION ??
+        process.env['AZURE_OPENAI_API_VERSION'],
+      config,
+      options,
+    } = params;
+
+    const modelInstance = AzureOpenAi({
+      apiKey,
+      resourceName,
+      deploymentName,
+      version,
+      config,
+      options,
+    });
+
+    return modelInstance;
+  }
+
+  async modelOllama(params: any, _pinsSettingsList: PinsSettings[], context: any) {
+    const { Ollama } = await eval(`import('llmclient')`);
+    const {
+      model,
+      url = context.privates.OLLAMA_SERVER
+        ? context.privates.OLLAMA_SERVER + '/v1'
+        : process.env['OLLAMA_SERVER']
+        ? process.env['OLLAMA_SERVER'] + '/v1'
+        : 'http://localhost:11434/v1',
+      apiKey,
+      config,
+      options,
+    } = params;
+
+    const modelInstance = Ollama({ model, url, apiKey, config, options });
+
+    return modelInstance;
   }
 
   async generate(params: any, _pinsSettingsList: PinsSettings[], context: any) {
@@ -38,6 +89,15 @@ class DspService {
 
 export const model = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
   new DspService().model(params, pinsSettingsList, context);
+
+export const modelOpenAI = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new DspService().modelOpenAI(params, pinsSettingsList, context);
+
+export const modelAzureOpenAi = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new DspService().modelAzureOpenAi(params, pinsSettingsList, context);
+
+export const modelOllama = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new DspService().modelOllama(params, pinsSettingsList, context);
 
 export const generate = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
   new DspService().generate(params, pinsSettingsList, context);
