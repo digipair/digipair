c217c1e9f3feecb5a16568941a1b68148094519a
Marc BUILS <marc.buils@presta.tessi.fr>
2024-07-09 18:13:45 +0200

    feat: add dsp agents

--- libs/skill-dsp/src/lib/skill-dsp.ts
+++ libs/skill-dsp/src/lib/skill-dsp.ts
@@ -1,9 +1,35 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { PinsSettings, executePinsList } from '@digipair/engine';
+import {
+  AxAI,
+  AxAIOpenAI,
+  AxAIAzureOpenAI,
+  AxAIOllama,
+  AxGenerate,
+  AxChainOfThought,
+  AxAgent,
+  AxFunction,
+  AxGenerateOptions,
+  AxReAct,
+} from '@ax-llm/ax';
 
 class DspService {
+  private async prepareFunctions(functions: AxFunction[], context: any): Promise<AxFunction[]> {
+    return await Promise.all(
+      functions.map(async f => {
+        if ((f as any).library && (f as any).element) {
+          return await executePinsList([f as PinsSettings], context);
+        }
+
+        return {
+          ...f,
+          func: async () => await executePinsList(f.func as any as PinsSettings[], context),
+        };
+      }),
+    );
+  }
+
   async model(params: any, _pinsSettingsList: PinsSettings[], _context: any) {
-    const { AxAI } = await eval(`import('@ax-llm/ax')`);
     const { name, options } = params;
 
     const modelInstance = new AxAI({ name, ...options });
@@ -12,7 +38,6 @@ class DspService {
   }
 
   async modelOpenAI(params: any, _pinsSettingsList: PinsSettings[], context: any) {
-    const { AxAIOpenAI } = await eval(`import('@ax-llm/ax')`);
     const {
       apiKey = context.privates.OPENAI_API_KEY ?? process.env['OPENAI_API_KEY'],
       apiURL = context.privates.OPENAI_SERVER ?? process.env['OPENAI_SERVER'],
@@ -31,7 +56,6 @@ class DspService {
   }
 
   async modelAzureOpenAi(params: any, _pinsSettingsList: PinsSettings[], context: any) {
-    const { AxAIAzureOpenAI } = await eval(`import('@ax-llm/ax')`);
     const {
       apiKey = context.privates.AZURE_OPENAI_API_KEY ?? process.env['AZURE_OPENAI_API_KEY'],
       resourceName = context.privates.AZURE_OPENAI_API_INSTANCE_NAME ??
@@ -57,14 +81,13 @@ class DspService {
   }
 
   async modelOllama(params: any, _pinsSettingsList: PinsSettings[], context: any) {
-    const { AxAIOllama } = await eval(`import('@ax-llm/ax')`);
     const {
       model,
       url = context.privates.OLLAMA_SERVER
         ? context.privates.OLLAMA_SERVER + '/v1'
         : process.env['OLLAMA_SERVER']
-        ? process.env['OLLAMA_SERVER'] + '/v1'
-        : 'http://localhost:11434/v1',
+          ? process.env['OLLAMA_SERVER'] + '/v1'
+          : 'http://localhost:11434/v1',
       apiKey,
       config,
       options,
@@ -76,13 +99,75 @@ class DspService {
   }
 
   async generate(params: any, _pinsSettingsList: PinsSettings[], context: any) {
-    const { AxGenerate } = await eval(`import('@ax-llm/ax')`);
-    const { model = context.privates.MODEL_DSP, signature, input } = params;
+    const { model = context.privates.MODEL_DSP, functions = [], signature, input } = params;
 
     const modelInstance = await executePinsList(model, context);
-    const gen = new AxGenerate(modelInstance, signature);
+    const gen = new AxGenerate(modelInstance, signature, {
+      functions: await this.prepareFunctions(functions, context),
+    } as AxGenerateOptions);
     const result = await gen.forward(input);
 
+    return result;
+  }
+
+  async chainOfThought(params: any, _pinsSettingsList: PinsSettings[], context: any) {
+    const { model = context.privates.MODEL_DSP, functions = [], signature, input } = params;
+
+    const modelInstance = await executePinsList(model, context);
+    const gen = new AxChainOfThought(modelInstance, signature, {
+      functions: await this.prepareFunctions(functions, context),
+    });
+    const result = await gen.forward(input);
+
+    return result;
+  }
+
+  async react(params: any, _pinsSettingsList: PinsSettings[], context: any) {
+    const { model = context.privates.MODEL_DSP, functions = [], signature, input } = params;
+
+    const modelInstance = await executePinsList(model, context);
+    const gen = new AxReAct(modelInstance, signature, {
+      functions: await this.prepareFunctions(functions, context),
+    });
+    const result = await gen.forward(input);
+
+    return result;
+  }
+
+  async agent(params: any, _pinsSettingsList: PinsSettings[], context: any) {
+    const {
+      model = context.privates.MODEL_DSP,
+      functions = [],
+      agents = [],
+      forward = true,
+      name,
+      description,
+      signature,
+      input,
+    } = params;
+
+    const modelInstance = await executePinsList(model, context);
+    const agent = new AxAgent(modelInstance, {
+      name,
+      description,
+      signature,
+      functions: await this.prepareFunctions(functions, context),
+      agents: await Promise.all(
+        agents.map(
+          async (execute: PinsSettings) =>
+            await executePinsList(
+              [{ ...execute, properties: { ...execute.properties, forward: false } }],
+              context,
+            ),
+        ),
+      ),
+    });
+
+    if (!forward) {
+      return agent;
+    }
+
+    const result = await agent.forward(input);
     return result;
   }
 }
@@ -101,3 +186,12 @@ export const modelOllama = (params: any, pinsSettingsList: PinsSettings[], conte
 
 export const generate = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
   new DspService().generate(params, pinsSettingsList, context);
+
+export const chainOfThought = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new DspService().chainOfThought(params, pinsSettingsList, context);
+
+export const react = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new DspService().react(params, pinsSettingsList, context);
+
+export const agent = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new DspService().agent(params, pinsSettingsList, context);
