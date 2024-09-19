496109abaa3e26d51269db7a610a88b69b25bfe2
Marc BUILS <marc.buils@presta.tessi.fr>
2024-09-19 15:44:34 +0200

    feat: add boost by step management

--- libs/skill-chatbot/src/lib/skill-chatbot.ts
+++ libs/skill-chatbot/src/lib/skill-chatbot.ts
@@ -15,6 +15,15 @@ class ChatbotService {
     };
   }
 
+  async execute(params: any, _pinsSettingsList: PinsSettings[], context: any) {
+    const { step } = params;
+    const { steps } = context.boost;
+    const execute = steps.findIndex(({ name }: any) => name === step);
+
+    const result = await executePinsList(execute, context);
+    return result;
+  }
+
   // SCENES
   async boost(params: any, _pinsSettingsList: PinsSettings[], context: any) {
     if (context.request.method !== 'POST') {
@@ -25,10 +34,35 @@ class ChatbotService {
     const result = await executePinsList(execute, context);
     return result;
   }
+
+  async boostBySteps(params: any, _pinsSettingsList: PinsSettings[], context: any): Promise<any> {
+    if (context.request.method !== 'POST') {
+      return { error: 'Method not allowed' };
+    }
+
+    const { steps } = params;
+    const step = context.request.body.step
+      ? steps.findIndex(({ name }: any) => name === context.request.body.step)
+      : 0;
+    const execute = steps[step];
+
+    const result = await executePinsList(execute, { ...context, boost: { steps } });
+    return result;
+  }
 }
+
+// PIN'S
+
+export const answer = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new ChatbotService().answer(params, pinsSettingsList, context);
+
+export const execute = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new ChatbotService().execute(params, pinsSettingsList, context);
+
+// SCENES
 
 export const boost = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
   new ChatbotService().boost(params, pinsSettingsList, context);
 
-export const answer = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
-  new ChatbotService().answer(params, pinsSettingsList, context);
+export const boostBySteps = (params: any, pinsSettingsList: PinsSettings[], context: any) =>
+  new ChatbotService().boost(params, pinsSettingsList, context);
