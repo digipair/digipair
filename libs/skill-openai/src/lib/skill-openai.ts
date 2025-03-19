3448a1f242c03949743d4e4e3fca0789136bbdda
Marc BUILS <marc.buils@presta.tessi.fr>
2025-03-19 11:04:15 +0100

    feat: update default embedding size

--- libs/skill-openai/src/lib/skill-openai.ts
+++ libs/skill-openai/src/lib/skill-openai.ts
@@ -67,7 +67,7 @@ class OpenAIService {
   async embeddingsAzure(params: any, _pinsSettingsList: PinsSettings[], context: any) {
     const {
       deploymentName,
-      dimensions = 1024,
+      dimensions = 1536,
       openAIApiKey = context.privates.AZURE_OPENAI_API_KEY ?? process.env['AZURE_OPENAI_API_KEY'],
       openAIApiInstanceName = context.privates.AZURE_OPENAI_API_INSTANCE_NAME ??
         process.env['AZURE_OPENAI_API_INSTANCE_NAME'],
