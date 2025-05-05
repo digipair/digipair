4dbf863a5d3ca846b151bc9c8ebbee2252d3fce0
Marc BUILS <marc.buils@presta.tessi.fr>
2025-05-05 15:36:16 +0200

    fix: bad sharp import

--- libs/skill-sharp/src/lib/skill-sharp.ts
+++ libs/skill-sharp/src/lib/skill-sharp.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { PinsSettings } from '@digipair/engine';
-import * as sharp from 'sharp';
+import sharp from 'sharp';
 
 type Sharp = any;
 
