aaf84849eb77db94723c77a3b06b7d193337d184
Diego <diego@MacBook-Air-de-Marc.local>
2025-07-11 17:36:28 +0200

    feat: skill-web externals

--- libs/skill-web/rollup.config.cjs
+++ libs/skill-web/rollup.config.cjs
@@ -1,20 +1,34 @@
-const { withNx } = require('@nx/rollup/with-nx');
+ const { withNx } = require('@nx/rollup/with-nx');
 
-module.exports = withNx(
-  {
-    main: './src/index.ts',
-    outputPath: './dist',
-    tsConfig: './tsconfig.lib.json',
-    compiler: 'swc',
-    format: ['esm', "cjs"],
-    assets: [
-      {
-        input: 'libs/skill-web/src/',
-        glob: '*.json',
-        output: '.'
-      }
-    ]
+module.exports = withNx({
+  main: './src/index.ts',
+  outputPath: './dist',
+  tsConfig: './tsconfig.lib.json',
+  compiler: 'swc',
+  format: ['esm', 'cjs'],
+  assets: [
+    {
+      input: 'libs/skill-web/src/',
+      glob: '*.json',
+      output: '.',
+    },
+  ],
+  external: [
+    'fs', 'path', 'vm', 'url', 'jsdom',
+    '@digipair/engine',
+    '@digipair/skill-basic',
+    '@digipair/skill-service',
+    '@digipair/skill-chatbot',
+    '@digipair/skill-dsp',
+    '@digipair/skill-sse',
+    '@digipair/skill-common',
+    '@digipair/skill-debug',
+    '@digipair/skill-web',
+    '@digipair/skill-web-chatbot',
+    '@digipair/skill-web-inputs',
+  ],
   },
+
   {
     // Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
     // e.g.
