5dcd0d9c89e3b02d710dce5c0d22aa5437612cdf
dgreboval <dgreboval@gestform.com>
2026-03-20 12:08:29 +0100

    add cancel management, workerOptions

--- libs/skill-temporal/src/lib/activities.ts
+++ libs/skill-temporal/src/lib/activities.ts
@@ -1,4 +1,5 @@
 import { executePinsList as executePinsListEngine, PinsSettings } from '@digipair/engine';
+import { heartbeat, cancellationSignal, activityInfo } from '@temporalio/activity';
 
 export async function executePinsList({
   pinsSettingsList,
@@ -7,5 +8,26 @@ export async function executePinsList({
   pinsSettingsList: PinsSettings[];
   context: any;
 }): Promise<string> {
-  return await executePinsListEngine(pinsSettingsList, context);
+  const info = activityInfo();
+  const abortSignal = cancellationSignal();
+  const abortController = new AbortController();
+  const newContext = {...context, protected : {...context.protected, signal: abortController.signal }}
+
+  abortSignal.addEventListener('abort', () => {
+    abortController.abort();
+  });
+
+  const intervalMs = info.heartbeatTimeoutMs
+    ? Math.floor(info.heartbeatTimeoutMs / 3 / 1000) * 1000
+    : 3000;
+  const interval = setInterval(() => {
+    heartbeat();
+  }, intervalMs);
+
+  try {
+    return await executePinsListEngine(pinsSettingsList, newContext);
+  } finally {
+    clearInterval(interval);
+    abortController.abort();
+  }
 }
